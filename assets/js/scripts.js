7facc6bb7f86ec6a4c0713f7bc9d1e0960f03712
plum3tt <117678044+plum3tt@users.noreply.github.com>
2026-05-11 11:13:17 -0400

    stuff
    asidhaosdaiud

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,159 +1,100 @@
-const galleries = {
-    Underdawg: {
-        images: [
-            '../assets/images/combat-robotics/Underdawg/1.jpg',
-            '../assets/images/combat-robotics/Underdawg/2.jpg',
-            '../assets/images/combat-robotics/Underdawg/3.jpg',
-            '../assets/images/combat-robotics/Underdawg/4.jpg',
-            '../assets/images/combat-robotics/Underdawg/5.jpg',
-            '../assets/images/combat-robotics/Underdawg/6.jpg',
-            '../assets/images/combat-robotics/Underdawg/7.jpg',
-            '../assets/images/combat-robotics/Underdawg/8.jpg'
-        ],
-        description: 'Underdawg is a 3lb beetle combat robot and was my first project at MIT and the reason for my pursuit in mechanical engineering.' +
-        ' The first version of the robot took most of my first semester to design as part of a group. Beyond that, each next iteration has been of my own design.' +
-        ' Underdawg has competed at its current iteration at 2 competitions, Build Fight Fun! in July of 2025 and NHRL in Nov of 2025.' +
-        ' Underdawg lost both of its matches at NHRL, but placed second at Build Fight Fun!'
-    },
-    Uppydawg: {
-        images: [
-            '../assets/images/combat-robotics/Uppydawg/1.jpg',
-            '../assets/images/combat-robotics/Uppydawg/2.jpg',
-            '../assets/images/combat-robotics/Uppydawg/3.jpg',
-            '../assets/images/combat-robotics/Uppydawg/4.jpg',
-            '../assets/images/combat-robotics/Uppydawg/5.jpg',
-            '../assets/images/combat-robotics/Uppydawg/6.jpg',
-            '../assets/images/combat-robotics/Uppydawg/7.jpg',
-            '../assets/images/combat-robotics/Uppydawg/8.jpg',
-            '../assets/images/combat-robotics/Uppydawg/9.jpg'
-        ],
-        description: 'Uppydawg is my second 1lb Plastic Antweight (Plant) combat robot made for a plant tournament at Northeastern University in the summer of 2025' +
-        ' Uppydawg is a full body robot that lifts opponents by getting under them with its slopped body and pressing a hidden arm on the floor below it to lift its body and the body of other robots.' +
-        ' This robot was a simple way to experiment with other weapon types in combat robotics and have fun while at it!'
-    },
-    DAYBREAK: {
-        images: [
-            '../assets/images/combat-robotics/DAYBREAK/1.jpeg',
-            '../assets/images/combat-robotics/DAYBREAK/2.jpeg',
-            '../assets/images/combat-robotics/DAYBREAK/3.jpeg',
-            '../assets/images/combat-robotics/DAYBREAK/4.jpeg',
-            '../assets/images/combat-robotics/DAYBREAK/5.jpeg',
-            '../assets/images/combat-robotics/DAYBREAK/6.jpeg',
-            '../assets/images/combat-robotics/DAYBREAK/7.jpeg',
-            '../assets/images/combat-robotics/DAYBREAK/8.jpeg'
-        ],
-        description: 'DAYBREAK is a 30lb robot that was designed and built within the two weeks before its competition date.' +
-        ' DAYBREAK was originally a flamethrower robot, but flamethrowers were banned at the NHRL competition, so I wanted to redesign DAYBREAK as a \'melty brain\' robot which spins its entire body rapidly to attack.' +
-        ' Given the time constraint, I used an existing welded frame and electronics for DAYBREAK.' + 
-        ' It was a fun experience to build a large bot in a short timeframe, though ultimately DAYBREAK was unable to compete as the drive system didn\'t have enough torque to spin DAYBREAK.'
-    },
-    Maxydawg: {
-        images: [
-            '../assets/images/combat-robotics/Maxydawg/1.jpeg',
-            '../assets/images/combat-robotics/Maxydawg/2.jpeg',
-            '../assets/images/combat-robotics/Maxydawg/3.jpeg',
-            '../assets/images/combat-robotics/Maxydawg/4.jpeg',
-            '../assets/images/combat-robotics/Maxydawg/5.jpeg',
-            '../assets/images/combat-robotics/Maxydawg/6.jpeg',
-            '../assets/images/combat-robotics/Maxydawg/7.jpeg',
-            '../assets/images/combat-robotics/Maxydawg/8.jpeg'
-        ],
-        description: 'Maxydawg is a 1lb Full Combat Antweight robot that I designed and built over the course of 13 hours to compete against against a friend building his robot in the same timeframe.' +
-        ' Maxydawg\'s horizontal spinning weapon is mounted a custom 3D printed \'hubmotor\'that mounts on a basic brushless motor.' +
-        ' Maxydawg ended about 200g overweight, but regardless we fought five matches total until Maxydawg was too damaged to repair with the opposing bot winning with a score of 3-2.'
-    },
-    Lildawg: {
-        images: [
-            '../assets/images/combat-robotics/Lildawg/1.jpeg',
-            '../assets/images/combat-robotics/Lildawg/2.jpeg',
-            '../assets/images/combat-robotics/Lildawg/3.jpeg',
-            '../assets/images/combat-robotics/Lildawg/4.jpeg'
-        ],
-        description: 'description' +
-        ' Lildawg was my first 1lb Plastic Antweight (Plant) and first multi-bot combat robot.' +
-        ' I built Lildawg originally to be single robot, but had no proper gauge of large it should be and ended up with a robot that was way underweight.' +
-        ' Instead of redesigning Lildawg, I decided to build a second copy of the robot because a multi-bot sounded more fun!'
-    },
-    GlassFlowers: {
-        images: [
-            '../assets/images/creativejuice/GlassFlowers/1.jpeg',
-            '../assets/images/creativejuice/GlassFlowers/2.jpeg',
-            '../assets/images/creativejuice/GlassFlowers/3.jpeg',
-            '../assets/images/creativejuice/GlassFlowers/4.jpeg',
-            '../assets/images/creativejuice/GlassFlowers/5.jpeg'
-        ],
-        description: 'One of the new hobbies I picked up at MIT has been glass lampworking!' +
-        ' I\'ve been making many flowers as I\'ve found them to be a great way to learn and practice techniques.' +
-        ' Most of my flowers are the same basic design, but I\'m starting to experiment making other kinds!'
-    },
-    GlassCritters: {
-        images: [
-            '../assets/images/creativejuice/GlassCritters/1.jpeg',
-            '../assets/images/creativejuice/GlassCritters/2.jpeg',
-            '../assets/images/creativejuice/GlassCritters/3.jpeg'
-        ],
-        description: 'The reason I picked up glass lampworking was because I\'d loved to make a collection of insects and other small critters, but they have proven much harder than I had thought they\'d be' +
-        ' I\'m currently focusing on making spiders over and over until I feel comfortable with the techniques involved.' +
-        ' The legs especially have proven difficult to make, but I think I\'ll be able to move on to more complex designs soon!'
-    },
-    CreativeMisc: {
-        images: [
-            '../assets/images/creativejuice/Misc/1.jpeg',
-            '../assets/images/creativejuice/Misc/2.jpeg',
-            '../assets/images/creativejuice/Misc/3.jpeg',
-            '../assets/images/creativejuice/Misc/4.jpeg'
-        ],
-        description: 'Here are some of the various random projects or gifts I\'ve made!'
-    },
-    SauceathonBot: {
-        images: [
-            '../assets/images/smartjuice/SauceathonBot/1.jpeg',
-            '../assets/images/smartjuice/SauceathonBot/2.jpg',
-            '../assets/images/smartjuice/SauceathonBot/3.jpeg'
-        ],
-        description: 'I participated at Sauceathon 2025 in Norwalk, Connecticut in a group with 5 others to build a first person view robot that would \'save children\' within an arena.' +
-        ' We had a total of 24 hours to design and build the robot, we weren\'t told the final task until we had completed some subtasks first.' +
-        ' Our robot was designed with the goal of completing a few high point tasks (saving \'high value\' children) rather than completing many low point tasks' + 
-        ' Our robot failed to perform as we wished, but it was my first real hardware hackathon and I had a blast participating!'
-    },
-    StrobeDemo: {
-        images: [
-            '../assets/images/smartjuice/StrobeDemo/1.jpeg',
-            '../assets/images/smartjuice/StrobeDemo/2.jpeg',
-            '../assets/images/smartjuice/StrobeDemo/3.jpeg'
-        ],
-        description: 'These are some simple containers for an adjustable strobe light and turntable that I had designed.' +
-        ' These were used as teaching materials in an extreme imaging class at MIT over the summer of 2025.'
-    },
-    Trident: {
-        images: [
-            '../assets/images/smartjuice/Trident/1.png',
-            '../assets/images/smartjuice/Trident/2.jpeg',
-            '../assets/images/smartjuice/Trident/3.png'
-        ],
-        description: 'Trident is a WIP robotic arm that will have a total of 3 diffrent kinds of gearboxes in it\s system; cycloidal, harmonic, and planetary.' +
-        ' This is my first long-term personal project beyond combat robotics and classes that I am pursuing for the sake of learning new techniques.' +
-        ' The arm is currently entirely 3D printed, but I would like to machine it out of metal in the future after having built a working 3D printed version.'
+
+const galleries = {};
+let pageData = null;
+
+async function loadProjectData() {
+    try {
+        const response = await fetch('assets/data/projects.json');
+        const data = await response.json();
+        pageData = data;
+
+        if (data.projects) {
+            Object.entries(data.projects).forEach(([id, project]) => {
+                galleries[id] = Object.assign({ name: project.title || id }, project);
+            });
+        }
+
+        buildProjectSections();
+        loadHomePhotos();
+    } catch (error) {
+        console.warn('Could not load project JSON', error);
     }
-    // ,
-    // UnloadingShip: {
-    //     images: [
-    //         '../assets/images/smartjuice/UnloadingShip/1.jpeg',
-    //         '../assets/images/smartjuice/UnloadingShip/2.jpeg',
-    //         '../assets/images/smartjuice/UnloadingShip/3.jpeg',
-    //         '../assets/images/smartjuice/UnloadingShip/4.png',
-    //         '../assets/images/smartjuice/UnloadingShip/5.png',
-    //         '../assets/images/smartjuice/UnloadingShip/6.jpeg',
-    //         '../assets/images/smartjuice/UnloadingShip/7.jpeg',
-    //         '../assets/images/smartjuice/UnloadingShip/8.jpeg',
-    //         '../assets/images/smartjuice/UnloadingShip/9.jpeg',
-    //     ],
-    //     description: 'description' +
-    //     ' ' +
-    //     ' ' + 
-    //     ' '
-    // }
-};
+}
+
+function getPageName() {
+    return window.location.pathname.split('/').pop().split('.').shift();
+}
+
+function buildProjectSections() {
+    if (!pageData || !pageData.pages) return;
+    const pageName = getPageName();
+    const pageConfig = pageData.pages[pageName];
+    if (!pageConfig) return;
+
+    const sectionsContainer = document.getElementById('project-sections');
+    if (!sectionsContainer) return;
+
+    sectionsContainer.innerHTML = '';
+
+    pageConfig.sections.forEach(section => {
+        const sectionEl = document.createElement('section');
+        const heading = document.createElement('h1');
+        heading.textContent = section.heading;
+        sectionEl.appendChild(heading);
+
+        const grid = document.createElement('div');
+        grid.className = 'gallery-grid';
+
+        section.projectIds.forEach(projectId => {
+            const project = pageData.projects?.[projectId];
+            if (!project) return;
+
+            const thumb = document.createElement('div');
+            thumb.className = 'project-thumb';
+            thumb.addEventListener('click', () => openGallery(projectId));
+
+            const container = document.createElement('div');
+            container.className = 'thumb-container';
+
+            const img = document.createElement('img');
+            img.src = project.thumb || (project.images && project.images[0]) || '';
+            img.alt = project.title || projectId;
+            img.loading = 'lazy';
+            container.appendChild(img);
+
+            const overlay = document.createElement('div');
+            overlay.className = 'thumb-overlay';
+            const titleSpan = document.createElement('span');
+            titleSpan.className = 'thumb-title';
+            titleSpan.textContent = project.title || projectId;
+            overlay.appendChild(titleSpan);
+            container.appendChild(overlay);
+
+            thumb.appendChild(container);
+            grid.appendChild(thumb);
+        });
+
+        sectionEl.appendChild(grid);
+        sectionsContainer.appendChild(sectionEl);
+    });
+}
+
+function loadHomePhotos() {
+    if (!pageData || !Array.isArray(pageData.homePhotos)) return;
+    const gallery = document.getElementById('home-photo-gallery');
+    if (!gallery) return;
+
+    gallery.innerHTML = '';
+    pageData.homePhotos.forEach((src, index) => {
+        const img = document.createElement('img');
+        img.src = src;
+        img.alt = `Photo ${index + 1}`;
+        img.loading = 'lazy';
+        gallery.appendChild(img);
+    });
+}
+
+loadProjectData();
 
 galleries.Trident = Object.assign(galleries.Trident || {}, {
     videos: [
@@ -362,5 +303,77 @@ function closeGallery() {
         const r = modalContent.querySelector('.arrow.right');
         if (l) l.remove();
         if (r) r.remove();
+    }
+}
+
+pdfjsLib.GlobalWorkerOptions.workerSrc =
+            'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js';
+
+const PDF_URL = 'assets/docs/resume.pdf';
+const SCALE = 2; // render at 2x for sharpness, CSS scales it down
+
+let pdfDoc = null;
+let currentPage = 1;
+let totalPages = 1;
+
+const container   = document.getElementById('resume-container');
+const pagesDiv    = document.getElementById('canvas-pages');
+const loadingDiv  = document.getElementById('resume-loading');
+const controls    = document.getElementById('page-controls');
+const pageInfo    = document.getElementById('page-info');
+const prevBtn     = document.getElementById('prev-btn');
+const nextBtn     = document.getElementById('next-btn');
+
+if (container && pagesDiv && loadingDiv && controls && pageInfo && prevBtn && nextBtn) {
+    async function renderPage(num) {
+        pagesDiv.innerHTML = '';
+
+        const page = await pdfDoc.getPage(num);
+        const viewport = page.getViewport({ scale: SCALE });
+
+        const wrap = document.createElement('div');
+        wrap.className = 'resume-canvas-wrap';
+
+        const canvas = document.createElement('canvas');
+        canvas.width  = viewport.width;
+        canvas.height = viewport.height;
+
+        wrap.appendChild(canvas);
+        pagesDiv.appendChild(wrap);
+
+        await page.render({
+            canvasContext: canvas.getContext('2d'),
+            viewport
+        }).promise;
+
+        pageInfo.textContent = `Page ${num} of ${totalPages}`;
+        prevBtn.disabled = num <= 1;
+        nextBtn.disabled = num >= totalPages;
+    }
+
+    pdfjsLib.getDocument(PDF_URL).promise.then(async (pdf) => {
+        pdfDoc     = pdf;
+        totalPages = pdf.numPages;
+
+        loadingDiv.style.display = 'none';
+        container.style.display  = 'flex';
+
+        if (totalPages > 1) controls.style.display = 'flex';
+
+        await renderPage(currentPage);
+    }).catch(() => {
+        loadingDiv.textContent = 'Could not load resume. Make sure resume.pdf is at assets/docs/resume.pdf';
+    });
+
+    prevBtn.addEventListener('click', async () => {
+        if (currentPage > 1) { currentPage--; await renderPage(currentPage); }
+    });
+
+    nextBtn.addEventListener('click', async () => {
+        if (currentPage < totalPages) { currentPage++; await renderPage(currentPage); }
+    });
+} else {
+    if (loadingDiv) {
+        loadingDiv.textContent = '';
     }
 }
